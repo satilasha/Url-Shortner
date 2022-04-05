95820b2cf936d3721f821c1fb4c60680734161f8
satilasha <swatighosh2111@gmail.com>
2022-04-05 11:55:56 +0530

    uniqueUrl

--- Controller/urlController.js
+++ Controller/urlController.js
@@ -3,51 +3,58 @@ const config = require('config')
 const UrlModel = require('../Model/UrlModel')
 const validator = require('../validator/validator')
 
-let createShortUrl = async (req,res) => {
-    try{
-    const {longUrl} = req.body
-    const baseUrl = config.get('baseUrl')
-    if (!validator.longUrlPresent('longUrl')) {
-        return res.status(400).send({ status: false, msg: "Please enter long Url" })
-    }   
-    if (!validator.isValidUrl(longUrl)) {
-        return res.status(400).send({ status: false, msg: ` 'Please give a valid longUrl' ` })
-    }
-    let url = await UrlModel.findOne({longUrl})
-    if(url){
-        return res.status(200).send({ status: true, data : url })
-    }else{
-        const urlCode = shortid.generate()
+let createShortUrl = async (req, res) => {
+    try {
+        let data = req.body
+        const { longUrl } = req.body
+        const baseUrl = config.get('baseUrl')
+        if (!validator.longUrlPresent(data)) {
+            return res.status(400).send({ status: false, msg: "Please enter long Url" })
+        }
+        if (!validator.isValidUrl(longUrl)) {
+            return res.status(400).send({ status: false, msg: ` 'Please give a valid longUrl' ` })
+        }
+        const urlCode = shortid.generate().toLowerCase()
         const shortUrl = baseUrl + '/' + urlCode
-        url = {
+        let url = {
             longUrl,
             shortUrl,
             urlCode
         }
+        let uniqueUrl = await UrlModel.findOne({ urlCode })
+        while (uniqueUrl) {
+            const urlCode = shortid.generate().toLowerCase()
+            const shortUrl = baseUrl + '/' + urlCode
+            url = {
+                longUrl,
+                shortUrl,
+                urlCode
+            }
+        }
 
         let newUrl = await UrlModel.create(url)
-        return res.status(200).send({ status: true, data : newUrl })
+        return res.status(200).send({ status: true, data: newUrl })
 
+    } catch (err) {
+        return res.status(400).send({ status: false, msg: err.message })
     }
-}catch(err){
-    return res.status(400).send({ status: false, msg : err.message})
-}
-   
+
 }
 
 
-let redirectUrl = async (req,res) => {
-    try{
-        const url = await UrlModel.findOne({urlCode : req.params.urlCode})
-        if(url){
+let redirectUrl = async (req, res) => {
+    try {
+        const url = await UrlModel.findOne({ urlCode: req.params.urlCode })
+        if (url) {
             return res.redirect(url.longUrl)
-        }else{
-            return res.status(404).send({status :false, msg : "no url found"})
+        } else {
+            return res.status(404).send({ status: false, msg: "no url found" })
         }
-    }catch(err){
-        return res.status(500).send({status :false, msg : err.message})
+    } catch (err) {
+        return res.status(500).send({ status: false, msg: err.message })
     }
 }
 module.exports = {
     createShortUrl,
-    redirectUrl}
+    redirectUrl
+}
